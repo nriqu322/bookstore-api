ec022032936591d9e46f29c29ffab0872ec9e0fa
Luis Saavedra <nriqu322@gmail.com>
2020-09-11 17:57:47 -0600

    book path

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
 
     if @book.save
-      render json: @book, status: :created, location: @book
+      render json: @book, status: :created, location: books_path(@book)
     else
       render json: @book.errors, status: :unprocessable_entity
     end
